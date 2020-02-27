5fca04293362039f761ae9772786a72017ecbcff
Autumn Martin <autumn.the.dev@gmail.com>
2020-02-27 15:58:38 -0700

    Update server initialization based on hapi docs -- & now startup message displays

--- src/server.js
+++ src/server.js
@@ -1,24 +1,29 @@
 import Hapi from 'hapi';
 
-// Create a new server instance & attach a new connection to it
-const server = new Hapi.Server({
-  host: 'localhost',
-  port: 8080
+const init = async () => {
+  // Create a new server instance & attach a new connection to it
+  const server = new Hapi.Server({
+    host: 'localhost',
+    port: 8080
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+      return 'Welcome!'
+    }
+  });
+
+  // Start the server
+  await server.start();
+
+  console.log(`Amazing server started at ${server.info.uri}`)
+}
+
+process.on('unhandledRejection', (err) => {
+  console.error('A wild bug was found!');
+  process.exit(1);
 });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, reply) => {
-    return 'Welcome!'
-  }
-});
-
-// Start the server
-server.start(err => {
-  if (err) {
-    console.error('A wild bug was found!');
-  }
-
-  console.log(`Server started at ${server.info.uri}`)
-});
+init();
