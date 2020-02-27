436d8f752cabddb3425d70f31dc13e9a80efbe06
Autumn Martin <autumn.the.dev@gmail.com>
2020-02-27 15:44:16 -0700

    Create first route & update to hapi 18.x syntax

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
 import Hapi from 'hapi';
 
-// Create a new server instance
-const server = new Hapi.Server();
-
-// Attach a new connection to it
-server.connection( {
+// Create a new server instance & attach a new connection to it
+const server = new Hapi.Server({
+  host: 'localhost',
   port: 8080
-})
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    return 'Welcome!'
+  }
+});
+
+// Start the server
+server.start(err => {
+  if (err) {
+    console.error('A wild bug was found!');
+  }
+
+  console.log(`Server started at ${server.info.uri}`)
+});
